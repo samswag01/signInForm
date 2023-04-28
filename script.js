b5b9e9c6a6c437fce34721e6633a89b0a75807cf
samswag01 <stnlrssl@gmail.com>
2023-04-28 20:29:37 +0100

    Final but first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let form = document.querySelector('.form'),
+    submitBtn = document.querySelector('input[type="submit"]'),
+    pwField = document.querySelector('input#password'),
+    pwConf = document.querySelector('input#password-conf'),
+    errorField = document.querySelector('.error');
+
+pwConf.addEventListener('input', (event) => {
+    if (event.target.value !== pwField.value) {
+        event.target.setCustomValidity('passwords do not match');
+        errorField.classList.add('active');
+    } else {
+        event.target.setCustomValidity('');
+        errorField.classList.remove('active')
+    }
+});
+
+document.querySelectorAll('.form__inner input').forEach(input => {
+    input.addEventListener('focus', event => {
+        event.target.classList.add('invalid-input');
+        event.target.removeEventListener();
+    });
+});
